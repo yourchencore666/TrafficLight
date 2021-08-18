8bb0b37b089dfaa5cf773a8e5dfc0e38b142d108
Artem Yurchenko <yourchencore@yandex.ru>
2021-08-18 18:01:47 +0200

    refactor logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -32,19 +32,16 @@ class ViewController: UIViewController {
     
     @IBAction func clickButton() {
         startButton.setTitle("NEXT", for: .normal)
-        if redView.alpha == 0.30000001192092896 && yellowView.alpha == 0.30000001192092896
-            && greenView.alpha == 0.30000001192092896 {
-            redView.alpha = 1
-        } else if yellowView.alpha == 0.30000001192092896 && redView.alpha == 1
-            && greenView.alpha == 0.30000001192092896 {
+        if redView.alpha == 1 {
+            redView.alpha = 0.3
             yellowView.alpha = 1
-            redView.alpha = 0.30000001192092896
-        } else if greenView.alpha == 0.30000001192092896 && yellowView.alpha == 1 {
-            redView.alpha = 0.30000001192092896
-            yellowView.alpha = 0.30000001192092896
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = 0.3
             greenView.alpha = 1
-        } else if greenView.alpha == 1 && redView.alpha == 0.30000001192092896 && yellowView.alpha == 0.30000001192092896 {
-            greenView.alpha = 0.30000001192092896
+        } else if greenView.alpha == 1 {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        } else {
             redView.alpha = 1
         }
     }
